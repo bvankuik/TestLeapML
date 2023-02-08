eac20d0d9aa4d2a508a98380cd364b5adc156bfd
Bart van Kuik <bart@dutchvirtual.nl>
2023-02-08 09:34:35 +0100

    Initial Commit

---
+++ TestLeapML/TestLeapMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLeapMLApp.swift
+//  TestLeapML
+//
+//  Created by Bart van Kuik on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestLeapMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
